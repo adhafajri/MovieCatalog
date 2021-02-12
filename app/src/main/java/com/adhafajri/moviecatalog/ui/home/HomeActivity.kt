901248ef2919981d1549a11eb947ea0ea4420530
Muhammad Adha Fajri Jonison <adhafajri@mhcasia.com>
2021-02-12 20:29:52 +0700

    Added Home Activity

--- app/src/main/java/com/adhafajri/moviecatalog/ui/home/HomeActivity.kt
+++ app/src/main/java/com/adhafajri/moviecatalog/ui/home/HomeActivity.kt
@@ -1,11 +1,12 @@
-package com.adhafajri.moviecatalog
+package com.adhafajri.moviecatalog.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.adhafajri.moviecatalog.R
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
     }
 }
