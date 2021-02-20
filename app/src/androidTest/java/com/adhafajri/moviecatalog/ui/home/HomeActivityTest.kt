3237c035db862ae54898d525c73525376dd17391
Muhammad Adha Fajri Jonison <adhafajri@mhcasia.com>
2021-02-20 10:51:31 +0700

    Added TV Show Instrumentation test

--- app/src/androidTest/java/com/adhafajri/moviecatalog/ui/home/HomeActivityTest.kt
+++ app/src/androidTest/java/com/adhafajri/moviecatalog/ui/home/HomeActivityTest.kt
@@ -10,6 +10,8 @@ import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.*
 import com.adhafajri.moviecatalog.R
+import com.adhafajri.moviecatalog.data.CatalogEntity
+import com.adhafajri.moviecatalog.utils.Constant
 import com.adhafajri.moviecatalog.utils.Data
 import org.hamcrest.Description
 import org.hamcrest.Matcher
@@ -18,8 +20,7 @@ import org.junit.Before
 import org.junit.Test
 
 class HomeActivityTest {
-    private val dummyMovieCatalog = Data.generateCatalogs()
-    private val dummyShowCatalog = Data.generateCatalogs()
+    private val dummyCatalog = Data.generateCatalogs()
     private val dummyPersons = Data.generatePersons()
     private val dummyTitles = Data.generateTitles()
 
@@ -33,18 +34,18 @@ class HomeActivityTest {
         onView(withId(R.id.rv_movie)).check(matches(isDisplayed()))
         onView(withId(R.id.rv_movie)).perform(
             RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(
-                dummyMovieCatalog.size
+                dummyCatalog.size
             )
         )
     }
 
     @Test
-    fun loadShowCatalogs() {
-        onView(withText("SHOWS")).perform(click())
+    fun loadTvShowCatalogs() {
+        onView(withText(R.string.tv_shows)).perform(click())
         onView(withId(R.id.rv_show)).check(matches(isDisplayed()))
         onView(withId(R.id.rv_show)).perform(
             RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(
-                dummyShowCatalog.size
+                dummyCatalog.size
             )
         )
     }
@@ -58,6 +59,13 @@ class HomeActivityTest {
             )
         )
 
+        val dummyMovieCatalog = ArrayList<CatalogEntity>()
+        for (catalog in dummyCatalog) {
+            if (catalog.type == Constant.MOVIE) {
+                dummyMovieCatalog.add(catalog)
+            }
+        }
+
         onView(withId(R.id.tv_catalog_title)).check(matches(isDisplayed()))
         onView(withId(R.id.tv_catalog_title)).check(matches(withText(dummyMovieCatalog[0].title)))
 
@@ -68,6 +76,53 @@ class HomeActivityTest {
 
         onView(withId(R.id.tv_overview)).check(matches(isDisplayed()))
         onView(withId(R.id.tv_overview_text)).check(matches(withText(dummyMovieCatalog[0].overview)))
+
+        onView(withId(R.id.scrollView)).perform(swipeUp())
+
+        onView(withId(R.id.rv_person)).check(matches(isDisplayed()))
+        onView(withId(R.id.rv_person)).perform(
+            RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(
+                dummyPersons.size
+            )
+        )
+
+        onView(withIndex(withId(R.id.rv_title), 0)).check(matches(isDisplayed()))
+        onView(withIndex(withId(R.id.rv_title), 0)).perform(
+            RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(
+                dummyTitles.size
+            )
+        )
+
+        onView(withId(R.id.btn_trailer)).perform(click())
+    }
+
+    @Test
+    fun loadDetailTvShowCatalog() {
+        onView(withText(R.string.tv_shows)).perform(click())
+        onView(withId(R.id.rv_show)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
+                0,
+                click()
+            )
+        )
+
+        val dummyTvShowCatalog = ArrayList<CatalogEntity>()
+        for (catalog in dummyCatalog) {
+            if (catalog.type == Constant.TV_SHOW) {
+                dummyTvShowCatalog.add(catalog)
+            }
+        }
+
+        onView(withId(R.id.tv_catalog_title)).check(matches(isDisplayed()))
+        onView(withId(R.id.tv_catalog_title)).check(matches(withText(dummyTvShowCatalog[0].title)))
+
+        onView(withId(R.id.tv_year)).check(matches(isDisplayed()))
+        onView(withId(R.id.tv_year)).check(matches(withText(dummyTvShowCatalog[0].year)))
+
+        onView(withId(R.id.img_poster)).check(matches(isDisplayed()))
+
+        onView(withId(R.id.tv_overview)).check(matches(isDisplayed()))
+        onView(withId(R.id.tv_overview_text)).check(matches(withText(dummyTvShowCatalog[0].overview)))
 
         onView(withId(R.id.scrollView)).perform(swipeUp())
 
